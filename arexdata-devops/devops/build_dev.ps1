7e372b96093b55f14bef02ec2811954907b380b1
zahid <Zahid>
2025-11-21 13:18:25 +0500

    files added

--- arexdata-devops/devops/build_dev.ps1
+++ arexdata-devops/devops/build_dev.ps1
@@ -2,10 +2,19 @@ param (
     [string] $build
 )
 
+if ($env:AREXDATA_BUILD_NUMBER) {
+    $build_number = $env:AREXDATA_BUILD_NUMBER
+} else {
+    $build_number = "4325"
+}
+
+$image_version = "$($env:RELEASE).$build_number.$($env:AREXDATA_REVISION_NUMBER)"
+
+Write-Host "Build Number: $build_number"
 # Dummy environment variables for demonstration
 $env:ENVIRONMENT = "dev"
 $env:RELEASE = "26.1.217"
-$env:AREXDATA_BUILD_NUMBER = "4325"
+
 $env:AREXDATA_REVISION_NUMBER = "0"
 
 function Write-Header() {
@@ -16,14 +25,14 @@ function Write-Header() {
 
 # Compute version
 $environment = $env:ENVIRONMENT
-$image_version = "$($env:RELEASE).$($env:AREXDATA_BUILD_NUMBER).$($env:AREXDATA_REVISION_NUMBER)"
+$image_version = "$($env:RELEASE).$build_number.$($env:AREXDATA_REVISION_NUMBER)"
 
+Write-Host "Build Number: $build_number"
 # Print configuration
 Write-Header "Configuration"
 Write-Host "Version: $image_version"
 Write-Host "Environment: $environment"
 Write-Host "Release: $($env:RELEASE)"
-Write-Host "Build Number: $($env:AREXDATA_BUILD_NUMBER)"
 Write-Host "Revision Number: $($env:AREXDATA_REVISION_NUMBER)"
 Write-Host "Params: " $PSBoundParameters
 Write-Host ""
